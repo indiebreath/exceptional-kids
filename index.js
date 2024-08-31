65b190390a2df66d11d3ffce870dcf60d4541aaf
indiebreath <indiebreath@proton.me>
2024-08-31 18:25:33 +1000

    Got Node working as expected

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 const port = 3000;
-const fs = require("fs");
 
 app.use("/", express.static(__dirname + "/source/pages"));
 app.use("/styles", express.static(__dirname + "/source/styles"));
 app.use("/images", express.static(__dirname + "/source/images"));
 
-app.get("/", (req, res) => {
-    res.type("text/plain");
-    res.status(200);
-
-    fs.readFile(__dirname + "/source/pages/index.html", function(err, data) {
-        res.send(data);
-    })
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
 });
 
+router.get("/", function(req, res) {
+    res.sendFile(__dirname + "/source/pages/index.html");
+});
+
+router.get("/about", function(req, res) {
+    res.sendFile(__dirname + "/source/pages/about.html");
+});
+
+router.get("/services", function(req, res) {
+    res.sendFile(__dirname + "/source/pages/services.html");
+});
+
+app.use("/", router);
 
 app.listen(port, () => {
     console.log("Application running and listening on port 3000.");
