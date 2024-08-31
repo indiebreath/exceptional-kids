2809a7757c841cc0249cfd555bcb3f4f5f8efdb7
indiebreath <indiebreath@proton.me>
2024-08-31 17:14:57 +1000

    Express final, it's stupid, but it works

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
 
-app.set("view engine", "ejs");
-
-app.use(express.static(__dirname + "/source/pages"));
+app.use("/", express.static(__dirname + "/source/pages"));
 app.use("/styles", express.static(__dirname + "/source/styles"));
 app.use("/images", express.static(__dirname + "/source/images"));
 
 app.get("/", (req, res) => {
     res.type("text/plain");
     res.status(200);
-    res.render(__dirname + "/source/pages/index.html");
+
+    fs.readFile(__dirname + "/source/pages/index.html", function(err, data) {
+        res.send(data);
+    })
 });
 
-app.get("/about", (req, res) => {
-    res.type("text/plain");
-    res.status(200);
-    res.render(__dirname + "/source/pages/about.html");
-});
-
-app.get("/services", (req, res) => {
-    res.type("text/plain");
-    res.status(200);
-    res.render(__dirname + "/source/pages/services.html");
-});
 
 app.listen(port, () => {
     console.log("Application running and listening on port 3000.");
